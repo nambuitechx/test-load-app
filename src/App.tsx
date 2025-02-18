b93b88278ca76b894c2834d3a0de8e6ac75f712a
nambuitechx <nam.bui@techxcorp.com>
2025-02-18 11:25:47 +0000

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import Header from './components/Header';
+
+// headers format
+/*
+[{ id: "string", key: "string", "value": "string" }]
+
+*/
+
+interface IHeader {
+  id: string;
+  key: string;
+  value: string;
+}
+
+interface IUpdateKeyHeader {
+  id: string;
+  newKey: string;
+}
+
+interface IUpdateValueHeader {
+  id: string;
+  newValue: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [headers, setHeaders] = useState<IHeader[]>([])
+
+  const addHeader = () => {
+    setHeaders([
+      ...headers,
+      { id: uuidv4(), key: "", value: "" }
+    ])
+  }
+
+  const updateKey = ({ id, newKey } : IUpdateKeyHeader) => {
+    const newHeaders = headers.map((header: IHeader) => {
+      if (header.id !== id) {
+        return header
+      } else {
+        return { id: header.id, key: newKey, value: header.value }
+      }
+    });
+    setHeaders(newHeaders)
+  }
+
+  const updateValue = ({ id, newValue } : IUpdateValueHeader) => {
+    const newHeaders = headers.map((header: IHeader) => {
+      if (header.id !== id) {
+        return header
+      } else {
+        return { id: header.id, key: header.key, value: newValue }
+      }
+    });
+    setHeaders(newHeaders)
+  }
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          URL: <input />
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <br />
+
+      <div>
+        <p>
+          Method: <input />
+        </p>
+      </div>
+      <br />
+
+      <div>
+        <button onClick={() => addHeader()}>Add header</button>
+      </div>
+      <div>
+        {headers.length > 0 && headers.map((header: any, index: number) => (
+          <div id={String(index)}>
+            <Header
+              id={header.id}
+              key={header.key}
+              value={header.value}
+              handleUpdateKey={updateKey}
+              handleUpdateValue={updateValue}
+            />
+          </div>
+        ))
+        }
+      </div>
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+function Header({ id, key, value, handleUpdateKey, handleUpdateValue } : { id: any, key: any, value: any, handleUpdateKey: any, handleUpdateValue: any }) {
+  return (
+    <div>
+      <p>
+        <span>Key: </span>
+        <input value={key} onChange={(e) => handleUpdateKey({ id, newKey: e.target.value })} />
+      </p>
+      <p>
+        <span>Value: </span>
+        <input value={value} onChange={(e) => handleUpdateValue({ id, newValue: e.target.value })} />
+      </p>
+    </div>
+  )
+}
+
+export default Header;
